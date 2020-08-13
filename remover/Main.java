d8a2acf4668ab75eef4bb2a4d16c4789f2163cd8
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-08-13 14:01:21 +0530

    Remove_oggy

--- remover/Main.java
+++ remover/Main.java
@@ -5,6 +5,16 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void removeOggy(ArrayList<String> names) {
+        
+         String subString = "oggy";
+			for(int i=names.size()-1; i>=0; i--)
+			{
+			   if(names.get(i).contains(subString))
+			   {
+			       System.out.println(names.get(i));
+			       names.remove(i);
+			   }
+			}
         return;
     }
     public static boolean oggyIsRemoved(ArrayList<String> names) {
